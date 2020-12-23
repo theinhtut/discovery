cf868dbdf835c9c7bba7b8dd3663fd63658e0708
Thein Htut <theinhtut.dec@gmail.com>
2020-12-23 16:52:58 +0800

    Learnt I2C

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -14,32 +14,48 @@ const IRA_REG_M: u8 = 0x0A;
 
 #[entry]
 fn main() -> ! {
-    let (i2c1, _delay, mut itm) = aux14::init();
+    let (i2c1, mut delay, mut itm) = aux14::init();
 
-    // Stage 1: Send the address of the register we want to read to the
-    // magnetometer
-    {
-        // TODO Broadcast START
+    loop {
+        // Broadcast START
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd1().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(1);
+            w.autoend().clear_bit()
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        // Wait until we can send more data
+        while i2c1.isr.read().txis().bit_is_clear() {}
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M
+        // Send the address of the register that we want to read: OUT_X_H_M
+        i2c1.txdr.write(|w| w.txdata().bits(OUT_X_H_M));
+
+        // Wait until the previous byte has been transmitted
+        while i2c1.isr.read().tc().bit_is_clear() {}
+
+        // Broadcast RESTART
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        i2c1.cr2.modify(|_, w| {
+            w.start().set_bit();
+            w.nbytes().bits(6);
+            w.rd_wrn().set_bit();
+            w.autoend().set_bit()
+        });
+
+        let mut buffer = [0u8; 6];
+        for byte in &mut buffer {
+            // Wait until we have received something
+            while i2c1.isr.read().rxne().bit_is_clear() {}
+
+            *byte = i2c1.rxdr.read().rxdata().bits();
+        }
+        // Broadcast STOP (automatic because of `AUTOEND = 1`)
+
+        iprintln!(&mut itm.stim[0], "{:?}", buffer);
+
+        delay.delay_ms(1_000_u16);
     }
-
-    // Stage 2: Receive the contents of the register we asked for
-    let byte = {
-        // TODO Broadcast RESTART
-
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
-
-        // TODO Receive the contents of the register
-
-        // TODO Broadcast STOP
-        0
-    };
-
-    // Expected output: 0x0A - 0b01001000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", IRA_REG_M, byte);
-
-    loop {}
 }
