899e053fad6e42c4e376bdf6a8f1216b6568c6f2
Thein Htut <theinhtut.dec@gmail.com>
2020-12-28 14:56:53 +0800

    Added lsm303agr experimental

--- src/15-led-compass/auxiliary/src/lib.rs
+++ src/15-led-compass/auxiliary/src/lib.rs
@@ -18,6 +18,8 @@ use f3::{
     Lsm303dlhc,
 };
 
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, UnscaledMeasurement};
+
 pub fn init() -> (Leds, Lsm303dlhc, Delay, ITM) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
@@ -41,4 +43,36 @@ pub fn init() -> (Leds, Lsm303dlhc, Delay, ITM) {
     let delay = Delay::new(cp.SYST, clocks);
 
     (leds, lsm303dlhc, delay, cp.ITM)
+}
+
+pub fn init_lsm303agr() -> (Leds, UnscaledMeasurement, Delay, ITM) {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let leds = Leds::new(gpioe);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+
+    let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+    // let lsm303agr = AccelOutputDataRate;
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    // sensor.into_mag_continuous();
+
+    let data = sensor.accel_data().unwrap();
+
+    let delay = Delay::new(cp.SYST, clocks);
+
+    (leds, data, delay, cp.ITM)
 }
--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -7,10 +7,10 @@ use aux15::{entry, iprint, iprintln, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    let (_leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
+    let (_leds, mut data, mut delay, mut itm) = aux15::init_lsm303agr();
 
     loop {
-        iprintln!(&mut itm.stim[0], "{:?}", lsm303dlhc.mag().unwrap());
+        iprintln!(&mut itm.stim[0], "x:{:?}, y:{:?}, z:{:?}", data.x, data.y, data.z);
         delay.delay_ms(1_000_u16);
     }
 }
